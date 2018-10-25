f13605d5ff24ed6c0865753583c6eeaaf2441fb0
KumarSunil17 <42208638+KumarSunil17@users.noreply.github.com>
2018-10-25 09:39:49 +0530

    modified

---
+++ app/src/main/java/com/kumarsunil17/tinstudent/fragments/Notice_Fragment.java
@@ -0,0 +1,36 @@
+package com.kumarsunil17.tinstudent.fragments;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kumarsunil17.tinstudent.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Notice_Fragment extends Fragment {
+    private View v;
+    private AppCompatActivity a;
+
+    public Notice_Fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        v = inflater.inflate(R.layout.fragment_notice_, container, false);
+        a = (AppCompatActivity) getActivity();
+        a.getSupportActionBar().setTitle("Notice");
+        NavigationView nav = a.findViewById(R.id.nav_view);
+        nav.setCheckedItem(R.id.nav_profile);
+        return v;
+    }
+
+}
