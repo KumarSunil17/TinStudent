7e7450400037044ec8808c508091b938f7b3d7ef
KumarSunil17 <42208638+KumarSunil17@users.noreply.github.com>
2018-11-14 20:41:25 +0530

    TeacherProfileActivity Added

--- app/src/main/java/com/kumarsunil17/tinstudent/StudentProfileActivity.java
+++ app/src/main/java/com/kumarsunil17/tinstudent/StudentProfileActivity.java
@@ -2,6 +2,7 @@ package com.kumarsunil17.tinstudent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class StudentProfileActivity extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class StudentProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_profile);
+
+    }
+
+    public void doCallStudent(View view) {
+    }
+
+    public void doMessageStudent(View view) {
+    }
+
+    public void doEmailStudent(View view) {
     }
 }
